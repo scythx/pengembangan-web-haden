10c9d5f808fef03c47b21f3bb49b1f24fb0186b7
Ananda Bayu <anandabayu090@gmail.com>
2020-11-04 13:25:28 +0700

    Edit Sport-League API Endpoint
    - misahin query ke .js terpisah (nambahin league-storage.js dan sport-storage.js)
    - semua API endpoint sudah berfungsi (sport & league)

---
+++ src/sport-storage.js
@@ -0,0 +1,33 @@
+import * as db from './database'
+
+const TABLE_NAME = 'sport'
+
+export async function deleteTable() {
+    return db.query(`DROP TABLE ${TABLE_NAME};`)
+}
+
+export async function getAll() {
+    const sports = await db.query(`SELECT * FROM ${TABLE_NAME};`)
+    return sports
+}
+
+export async function getOne(sportId) {
+    const sport = await db.query(`SELECT * FROM ${TABLE_NAME}
+        WHERE id_sport = $1;`, [sportId])
+    return sport
+}
+
+export async function createOne(name) {
+    db.query(`INSERT INTO ${TABLE_NAME} VALUES (DEFAULT, $1);`,
+        [name])
+}
+
+export async function updateOne(sportId, name) {
+    db.query(`UPDATE ${TABLE_NAME} 
+        SET name = $2 WHERE id_sport = $1;`
+        , [sportId, name])
+}
+
+export async function deleteOne(sportId) {
+    db.query(`DELETE FROM ${TABLE_NAME} WHERE id_sport = $1;`, [sportId])
+}
