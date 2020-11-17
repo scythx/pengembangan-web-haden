f67bccf1803be9ba7a917f0a44184b164e090aa6
hafizmfadli <lshafizmf1st@gmail.com>
2020-11-17 20:43:39 +0700

    Add list article's component

---
+++ src/frontend/src/components/UserArticle.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="container">
+            <img :src="imageUrl" alt="" class="responsive">
+            <h2>{{ title }}</h2>
+            <h3>{{ titleDesc }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'UserArticle',
+    props : ["imageUrl", "title", "titleDesc"],
+}
+</script>
+
+<style scoped>
+    .container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .responsive{
+        width: 50%;
+        height: auto;
+    }
+</style>
