00a6da9c38b921a6df4d9d14174c045e8bfa3611
Ananda Bayu <anandabayu090@gmail.com>
2020-12-12 15:13:35 +0700

    Replace bootstrap with vuetify component

--- src/frontend/src/components/Thumbnail.vue
+++ src/frontend/src/components/Thumbnail.vue
@@ -1,23 +1,50 @@
 <template>
-  <div id="container" >
-    <div v-for="(i, index) in mapped.slice(0,3)">
-      <hr>
-      <h1>{{getLeagueName(i.id_league)}}</h1>
-      <ul class="list-unstyled">
-        <div class="item-container">
-          <a href="#" v-for="(item, idx) in i.articles.slice(0,2)">
-            <li class="media">
-                <img v-bind:src="images[index+idx].url" class="ml-3" alt="...">
-                <div class="media-body">
-                  <h5 class="text-sm-left">{{item.title}}</h5>
-                </div>
-            </li>
-          </a>
-          <button type="button" class="btn btn-warning btn-lg btn-block">Lihat Semua</button>
-        </div> 
-      </ul>
+  <div id="container">
+    <div
+        v-for="(league,i) in leagues.slice(0,3)"
+        :key="i"
+    >
+        <div id="title" class="display-4 font-weight-light" v-text="league.name"></div>
+        <v-list 
+            dense
+            three-line
+            class="container"
+        >
+        <v-list-item
+            v-for="(item, index) in topheadline.slice(0,2)"
+            :key="index"
+            ripple
+            @click="() => {}"
+            class="tile mb-3"
+            dense
+        >
+           <v-img
+                :src="images[index].url"
+                class="mr-4 ml-5"
+                max-width="384"
+                min-width="384"
+            >
+            </v-img>
+            <v-list-item-content>
+                <h1
+                v-text="item.title"
+                class="h4 font-weight-light white--text"></h1>
+            </v-list-item-content>
+            
+        </v-list-item>
+        <v-btn
+            block
+            dense
+            elevation="2"
+            rounded
+            color="#FFCC00"
+            dark
+        >
+        <span id="lihatsemua" class="font-weight-light">Lihat Semua</span>
+        </v-btn>
+        <hr>
+        </v-list>
     </div>
-    <hr>
   </div>
 </template>
 
@@ -25,134 +52,67 @@
 import http from "@/http"
 
 export default {
-  name: 'Topheadline',
+  name: 'Thumbnail',
   components: {},
   data(){
       return{
         articles: [],
-        leagues: [],
-        mapped:[],
-        images:[]
+        topheadline:[],
+        images:[],
+        leagues:[]
       }
   },
   methods:{
-    getLeagueName(id) {
-      var i = 0
-      while(i < this.leagues.length){
-        if(this.leagues[i].id_league == id){
-          return this.leagues[i].name
-        }
-        i++
-      }
-    }
   },
   mounted(){
     http.get('/articles')
       .then((response) => {
         this.articles = response['data']
-        
-        http.get('/leagues')
-          .then((res) => {
-            this.leagues = res['data']
-            
-            
-            let temp = new Array()
-            var i = 0, j = 0
-
-            while(i < this.leagues.length){
-              while(j < this.articles.length){
-                if(this.leagues[i].id_league == this.articles[j].id_league){
-                    temp.push(this.articles[j])
-                }
-                j++
-              }
-
-              if(temp.length > 0){
-                var obj = new Object()
-                obj.id_league = this.leagues[i].id_league
-                obj.articles = temp
-                this.mapped.push(obj)
-                temp = new Array()
-              }
-
-              i++
-              j = 0
-            }
-          })
+        var i = 0
+        while(i < this.articles.length){
+          if(this.articles[i].is_headline){
+            this.topheadline.push(this.articles[i])
+          }
+          i++;
+        }
     })
 
     http.get('/images')
       .then((response) => {
         this.images = response['data']
     })
-  } 
+
+    http.get('/leagues')
+      .then((response) => {
+        this.leagues = response['data']
+    })
+  }
 }
 </script>
 
 <style scoped>
+#container, .container, .tile{
+  background: #222831;
+}
+#title{
+  margin-left: 200px;
+}
+.tile:hover {
+  background: #203E5F;
+}
+
+h1, .display-4{
+  color: #FFFFFF;
+}
+
 hr { 
   display: block;
-  margin-top: 0.5em;
-  margin-bottom: 0.5em;
-  margin-left: 5%;
-  margin-right: 5%;
+  margin-top: 1em;
   border-style: inset;
   border-width: 1px;
 }
 
-h1{
-  color: white;
-  margin-left: 8%;
-  margin-right: 5%;
-  font-family: 'Open Sans', sans-serif;
-  font-size:4vw;
-}
-
-#container{
-  width: 100%;
-  position: absolute;
-  background-color: #222831;
-}
-
-.item-container{
-  margin-left: 5%;
-  margin-right: 5%;
-  width: 90%;
-}
-
-.media{
-  margin-top: 1%;
-  align-items: center;
-  border-radius: 0.5vw;
-}
-
-.media:hover{
-  background-color: #203E5F;
-}
-
-.media-body{
-  margin-left:3%;
-  margin-right:3%;
-  color: white;
-  text-align: left;
-  font-family: 'Open Sans', sans-serif;
-}
-
-h5{
-  font-size:2.5vw;
-}
-
-img{
-  width: 40%;
-  height: 40%;
-  border-radius: 0.5vw;
-}
-
-button{
-  margin-top: 3%;
-  width: 98%;
-  margin-left: 1%;
-  font-family: 'Open Sans', sans-serif;
-  font-size: 2vw;
+#lihatsemua{
+    color: black;
 }
 </style>
