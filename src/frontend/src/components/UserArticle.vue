6115686417d9724208cf322e2fe719f0b619b078
Deonetwo <dewantoade11@gmail.com>
2020-12-18 19:26:28 +0700

    Fix Articles Backend & add share button

--- src/frontend/src/components/UserArticle.vue
+++ src/frontend/src/components/UserArticle.vue
@@ -1,24 +1,20 @@
 <template>
- <div>
-   <v-container>
+ <div id="container">
+   <v-container @click="onArticleClick(article.id_article)" class="tile">
      <!-- img -->
      <v-row>
        <v-col cols="12">
-         <v-img :src="src"></v-img>
+         <v-img src="https://picsum.photos/id/11/500/300"></v-img>
        </v-col>
      </v-row>
+     
      <!-- Title -->
      <div class="row">
-       <div class="col-12 text-h6 text-sm-h5 text-md-h4">
-         {{ title }}
+       <div class="col-12 text-h6 text-sm-h5 text-md-h2 white--text">
+         {{ article.title }}
        </div>
      </div>
-     <!-- Sub titel -->
-     <div class="row mt-n5">
-       <div class="col-12 text-caption text-sm-body-2 text-md-subtitle-1">
-         {{ description }}
-       </div>
-     </div>
+
    </v-container>
  </div>
 </template>
@@ -26,6 +22,24 @@
 <script>
  export default {
    name : 'UserArticle',
-   props : ["title", "description", "src"],
+   props : ["article"],
+   methods:{
+       onArticleClick(id){
+           if(this.$route.path !== '/article/'+id){
+               this.$router.push('/article/'+id)
+               this.$$router.go()
+           }
+       }
+   }
  }
 </script>
+
+<style scoped>
+ #container{
+   background-color: #222831;
+ }
+
+ .tile:hover{
+   background: #203E5F;
+ }
+</style>
