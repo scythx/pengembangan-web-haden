20ed13063e0ec7681a517def9456634c765e7f8b
Deonetwo <dewantoade11@gmail.com>
2020-12-18 19:26:28 +0700

    Fix Articles Backend

--- src/backend/src/article-storage.js
+++ src/backend/src/article-storage.js
@@ -4,16 +4,36 @@ const TABLE_NAME = 'article'
 export async function createTable(){
     let sql = `CREATE TABLE IF NOT EXISTS ${TABLE_NAME}(
         id_article bigserial NOT NULL,
+        thumbnail text COLLATE pg_catalog."default",
         title text COLLATE pg_catalog."default",
-        author text COLLATE pg_catalog."default",
+        id_author integer,
         content text COLLATE pg_catalog."default",
         date_published date,
         is_headline boolean,
         id_sport integer,
         id_league integer,
         id_team integer,
-        user_id integer,
-        CONSTRAINT article_pkey PRIMARY KEY (id_article)
+        CONSTRAINT article_pkey PRIMARY KEY (id_article),
+        CONSTRAINT league_fk FOREIGN KEY (id_league)
+            REFERENCES public.league (id_league) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID,
+        CONSTRAINT sport_fk FOREIGN KEY (id_sport)
+            REFERENCES public.sport (id_sport) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID,
+        CONSTRAINT team_fk FOREIGN KEY (id_team)
+            REFERENCES public.team (id_team) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID,
+        CONSTRAINT user_fk FOREIGN KEY (id_author)
+            REFERENCES public.users (id_user) MATCH SIMPLE
+            ON UPDATE NO ACTION
+            ON DELETE NO ACTION
+            NOT VALID
     );`;
 
     return db.query(sql);
