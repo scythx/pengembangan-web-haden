77923411dde24f49053e50ca5cfbe5d41a35b3ae
Raefaldhi Amartya Junior <scythx@localhost.localdomain>
2021-02-15 06:04:43 +0700

    Fix bug in theme

--- src/frontend/src/plugins/vuetify.js
+++ src/frontend/src/plugins/vuetify.js
@@ -4,15 +4,4 @@ import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({
-  theme:{
-    dark: true,
-    themes: {
-      dark: {
-        primary: '#1A2634',
-        secondary: '#FFCC00',
-        background: '#222831'
-      }
-    }
-  }
-})
+export default new Vuetify({})
